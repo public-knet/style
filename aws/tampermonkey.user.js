8ed5fc7a1df4cb976cb47c71e212037a53df56d4
nofaterock <nofaterock@gmail.com>
2025-08-08 20:17:49 +0900

    테스트

---
+++ aws/tampermonkey.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         AWS Console
+// @namespace    https://www.knetbiz.com
+// @version      1.0
+// @description  AWS Console 스타일 변경
+// @author       nofaterock
+// @updateURL    https://raw.githubusercontent.com/public-knet/style/main/aws/script.user.js
+// @downloadURL  https://raw.githubusercontent.com/public-knet/style/main/aws/script.user.js
+// @match        *://*.console.aws.amazon.com/*
+// @require      https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js
+// @grant        GM.xmlHttpRequest
+// @connect      public-knet-style.knetdev.workers.dev
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	document.addEventListener("DOMContentLoaded", function () {
+		inject('style', 'https://public-knet-style.knetdev.workers.dev/aws/custom.css');
+		inject('script', 'https://public-knet-style.knetdev.workers.dev/aws/custom.js');
+	});
+})();
+
+function inject(type, url) {
+	GM.xmlHttpRequest({
+		method: "GET",
+		url: url,
+		onload: res => {
+			console.log(`Inject KNET ${type}: ${url}`)
+			if (type === 'style') {
+				const css = document.createElement('style');
+				css.setAttribute("type", "text/css");
+				css.setAttribute("author", "KNET");
+				css.textContent = res.responseText;
+				document.body.parentNode.insertBefore(css, document.body.nextSibling);
+			} else if (type === 'script') {
+				var script = document.createElement("script");
+				script.textContent = res.responseText;
+				document.body.parentNode.insertBefore(script, document.body.nextSibling);
+			}
+		}
+	});
+}
