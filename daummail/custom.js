8bcc36b977ba30bc2afcfe870636fdbefda270b9
nofaterock <nofaterock@gmail.com>
2025-10-21 11:20:02 +0900

    Daum메일

---
+++ daummail/custom.js
@@ -0,0 +1,39 @@
+KNET.pretty = ($li) => {
+	if ($li.data('pretty')) {
+		return;
+	}
+
+	const $link = $li.find('a.link_mail');
+	const $linkText = $link.find('on_link');
+
+	// title 변경
+	let title = $linkText.text().trim();
+	if (!title.startsWith('--')) {
+		$li.data('pretty', true)
+		return;
+	}
+
+	title = title.split(' ')[1] + ' ';
+	$linkText.addClass('seperator').html(title.padEnd(30, '-'))
+
+	$li.data('pretty', true)
+}
+
+KNET.doPretty = async () => {
+	const $menuGroup1 = await KNET.util.waitForElement('#menuGroup1');
+
+	$menuGroup1.find('> li').each(function () {
+		KNET.pretty($(this));
+	});
+}
+
+// ----------------------------------------------------------------
+
+(async () => {
+	await KNET.util.loadJQuery();
+	await KNET.util.waitForDocumentReady();
+
+	// 최초 1회 실행
+	console.log('> Pretty : init')
+	await KNET.doPretty();
+})();
---
+++ daummail/tampermonkey.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         DaumMail
+// @namespace    https://github.com/public-knet/style
+// @version      1.0
+// @description  Daum메일 스타일 변경
+// @author       한승룡
+// @icon         https://mail.daum.net/favicon_daum.ico
+// @updateURL    https://raw.githubusercontent.com/public-knet/style/main/daummail/tampermonkey.user.js
+// @downloadURL  https://raw.githubusercontent.com/public-knet/style/main/daummail/tampermonkey.user.js
+// @match        *://mail.daum.net/top*
+// @grant        GM.xmlHttpRequest
+// @connect      raw.githubusercontent.com
+// ==/UserScript==
+
+(function () {
+	'use strict';
+
+	window.addEventListener('load', () => {
+		inject('script', 'https://raw.githubusercontent.com/public-knet/style/main/_commons/commons.js');
+		inject('style', 'https://raw.githubusercontent.com/public-knet/style/main/daummail/custom.css');
+		inject('script', 'https://raw.githubusercontent.com/public-knet/style/main/daummail/custom.js');
+	});
+})();
+
+function inject(type, url) {
+	GM.xmlHttpRequest({
+		method: "GET",
+		url: url,
+		onload: res => {
+			console.log(`Inject KNET ${type}: ${url}`)
+			if (type === 'style') {
+				const css = document.createElement('style');
+				css.setAttribute("author", "KNET");
+				css.setAttribute("type", "text/css");
+				css.textContent = res.responseText;
+				document.body.parentNode.insertBefore(css, document.body.nextSibling);
+			} else if (type === 'script') {
+				const script = document.createElement("script");
+				script.setAttribute("author", "KNET");
+				script.textContent = res.responseText;
+				document.body.parentNode.insertBefore(script, document.body.nextSibling);
+			}
+		}
+	});
+}
